e82c43b8bc452e01056a0e9f1e57bd41480095b6
izadoratoledo <izadora.toledo@doc9.com.br>
2024-05-20 20:56:58 -0300

    Criando banco de dados e tabela faturas

---
+++ banco.py
@@ -0,0 +1,23 @@
+﻿import sqlite3
+
+# Conecta ao banco de dados SQLite
+conn = sqlite3.connect('faturas.db')
+
+# Cria um cursor para executar comandos SQL
+cursor = conn.cursor()
+
+# Cria a tabela de faturas com as colunas especificadas
+cursor.execute('''CREATE TABLE faturas (
+                    ID TEXT PRIMARY KEY,  
+                    DueDate TEXT,        
+                    InvoiceNo INTEGER,   
+                    InvoiceDate TEXT,    
+                    CompanyName TEXT,    
+                    TotalDue REAL        
+                    )''')
+
+# Salva as alterações
+conn.commit()
+
+# Fecha a conexão com o banco de dados
+conn.close()
