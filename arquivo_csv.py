e51894faa29c1e6b9f97a9b87e4a232897c49a7a
izadoratoledo <izadora.toledo@doc9.com.br>
2024-05-22 20:00:41 -0300

    inserindo dados no banco e criando arquivo csv

---
+++ arquivo_csv.py
@@ -0,0 +1,46 @@
+﻿import os
+import sqlite3
+import csv
+from datetime import datetime
+
+# Conectar-se ao banco de dados SQLite
+conn = sqlite3.connect('faturas.db')
+cursor = conn.cursor()
+
+# Definir o nome do arquivo CSV
+nome_arquivo_csv = 'faturas_vencidas.csv'
+
+# Obter o diretório de downloads padrão do Windows
+diretorio_downloads = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+# Caminho completo do arquivo CSV
+caminho_arquivo_csv = os.path.join(diretorio_downloads, nome_arquivo_csv)
+
+# Abrir o arquivo CSV em modo de escrita
+with open(caminho_arquivo_csv, 'w', newline='') as csvfile:
+    # Criar o objeto escritor CSV
+    csv_writer = csv.writer(csvfile)
+
+    # Escrever o cabeçalho do arquivo CSV
+    csv_writer.writerow(['ID', 'Due Date', 'Invoice URL'])
+
+    # Obter a data atual
+    hoje = datetime.now().strftime('%Y-%m-%d')
+
+    # Consultar o banco de dados para obter apenas as faturas cuja data de vencimento é hoje ou passada
+    cursor.execute("SELECT ID, DueDate, InvoiceURL FROM faturas WHERE DueDate <= ?", (hoje,))
+    
+    faturas_vencidas = cursor.fetchall()
+
+    # Iterar sobre as faturas vencidas e escrever no arquivo CSV
+    for fatura in faturas_vencidas:
+        # Formatar a data para 'dd-mm-yyyy'
+        fatura_com_data_formatada = (fatura[0], datetime.strptime(fatura[1], '%Y-%m-%d').strftime('%d-%m-%Y'), fatura[2])
+        # Escrever os dados no arquivo CSV
+        csv_writer.writerow(fatura_com_data_formatada)
+
+# Fechar a conexão com o banco de dados
+conn.close()
+
+# Exibir o caminho completo do arquivo CSV criado
+print(f'Arquivo CSV "{nome_arquivo_csv}" criado com sucesso em: {caminho_arquivo_csv}')
---
+++ inserir_dados.py
@@ -0,0 +1,41 @@
+﻿import sqlite3
+
+# Conecta ao banco de dados SQLite
+conn = sqlite3.connect('faturas.db')
+
+# Cria um cursor para executar comandos SQL
+cursor = conn.cursor()
+
+# Dados fictícios para serem inseridos
+faturas_dados = [
+    ('eyinr1f4ek7v6ggdd6x90d', '2024-04-26', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/10.jpg'),
+    ('pjbpkw7sb2e5ydrgddy59', '2024-07-18', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/2.jpg'),
+    ('b7vmzeu68egqmmljtau90i', '2024-04-12', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/11.jpg'),
+    ('m9gf85a4qve9vc7ks1i41t', '2024-06-04', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/6.jpg'),
+    ('pxrc2nmcgzr0ji9uzr7kv7', '2024-05-27', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/5.jpg'),
+    ('gbh1nyjhk9jprejg45un4', '2024-07-20', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/3.jpg'),
+    ('phe5wzq3xa76r36gv6m0e', '2024-05-15', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/8.jpg'),
+    ('5ofji3nmpe2e1mg0to1tku', '2024-05-24', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/4.jpg'),
+    ('na7hefmqqbscl1hkvyt4k4', '2024-04-08', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/7.jpg'),
+    ('7i7lqvqrcbhstwegmazmh', '2024-05-05', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/12.jpg'),
+    ('bxt0ej8kmk726dmo6xck1i', '2024-04-26', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/9.jpg'),
+    ('bl8f16jd1pnaeyqxrbyzd', '2024-07-18', 10021, '13-02-2019', 'Sit Amet Corp.', 1234.40, 'rpachallengeocr.azurewebsites.net/invoices/1.jpg')   
+]
+
+# Instrução SQL para inserir dados na tabela
+sql = '''INSERT INTO faturas (ID, DueDate, InvoiceNo, InvoiceDate, CompanyName, TotalDue, InvoiceURL)
+         VALUES (?, ?, ?, ?, ?, ?, ?)'''
+
+# Insere cada registro na tabela faturas
+for fatura in faturas_dados:
+    try:
+        cursor.execute(sql, fatura)
+    except sqlite3.IntegrityError:
+        # Se a chave primária (ID) já existir, ignora o erro e não insere o registro duplicado
+        print(f"Registro com ID {fatura[0]} já existe.")
+
+# Salva as alterações
+conn.commit()
+
+# Fecha a conexão com o banco de dados
+conn.close()
